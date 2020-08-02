9056633985e714ba8b810e204eda7769dddf9e82
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-08-02 15:03:34 +0800

    fix: simplify route logic

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import path from 'path';
-import index from './routes/home';
-import about from './routes/about';
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
-app.use('/', index);
-app.use('/about', about);
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
 
 app.listen(3000);
--- src/routes/about.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('about');
-});
-
-export default router;
