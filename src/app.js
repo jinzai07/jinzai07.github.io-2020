02476c196340cd8c3eb8af55753c3a40980bd7bc
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-08-02 16:02:09 +0800

    feat: add local css setup

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import path from 'path';
 
 const app = express();
 
+/**
+ * Set up styles and EJS templating engine.
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+app.use('/public',express.static(path.join(__dirname,'static')));
 
+/**
+ * Initialize App routes.
+ */
 app.get('/', (req, res) => res.render('index'));
 app.get('/about', (req, res) => res.render('about'));
 
