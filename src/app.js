df8a5e02648315d4e02357f22cb525817599043d
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-08-05 20:12:40 +0800

    wip: implement random quote generator api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import axios from 'axios';
 
 const app = express();
 
@@ -13,7 +14,12 @@ app.use('/public',express.static(path.join(__dirname, 'static')));
 /**
  * Initialize App routes.
  */
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  const navLinks = ['Awards', 'About', 'Contact'];
+  const quoteApi = 'https://quotes.rest/qod';
+  return res.render('index', { navLinks, quote: 'Great ambition is the passion of a great character.  Those endowed with it may perform very good or very bad acts.  All depends on the principals which direct them.' });
+  axios.get(quoteApi).then(quote => res.render('index', { navLinks, quote: quote.data.contents.quotes[0].quote }));
+});
 app.get('/about', (req, res) => res.render('about'));
 
 app.listen(3000);
