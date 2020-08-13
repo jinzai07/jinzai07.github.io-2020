c304793c933bc707d7c9330a59b7f4a4dc5d6e21
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-08-13 20:20:10 +0800

    feat: finalize homepage

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -7,24 +7,50 @@ window.addEventListener("resize", () => {
   }, 400);
 });
 
-let i = 0;
-const text = 'Hi, I\'m Shinji.';
-const speed = 120;
+runTypeWriter = () => {
+  let i = 0;
+  const text = 'Hi, I\'m Shinji.';
+  const speed = 120;
 
-const typeWriter = () => {
-  const introDiv = document.querySelector("#intro");
+  const typeWriter = () => {
+    const introDiv = document.querySelector("#intro");
 
-  if (i < text.length) {
-    introDiv.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  } else {
-    setTimeout(() => {
-      introDiv.innerHTML = '';
-      i = 0;
-      typeWriter();
-    }, 3000);
+    if (i < text.length) {
+      introDiv.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      setTimeout(() => {
+        introDiv.innerHTML = '';
+        i = 0;
+        typeWriter();
+      }, 3000);
+    }
   }
+  typeWriter();
 }
 
-typeWriter();
+rocketFly = () => {
+  const rocket = document.querySelector('#rocket');
+
+  rocket.addEventListener('click', () => {
+    rocket.classList.toggle('fly');
+  });
+}
+
+navLinkGrow = () => {
+  const icons = document.querySelectorAll('.icon');
+
+  icons.forEach((icon) => {
+    icon.addEventListener('mouseover', () => {
+      icon.classList.add('hover');
+    });
+    icon.addEventListener('mouseout', () => {
+      icon.classList.remove('hover');
+    });
+  });
+}
+
+rocketFly();
+runTypeWriter();
+navLinkGrow();
