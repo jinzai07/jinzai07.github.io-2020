d0076c58211e85e6f2ba84778e4258749f9965f2
Shinji Yumikura <shinji.yumikura@ffuf.de>
2020-08-16 15:25:01 +0800

    fix: outsource random quote generator

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
 
 import { navLinks } from '../src/shared/constants/navigation-links'
+import { generateQuote } from './shared/extensions/random-quote-generator';
+import { sendEmail } from './shared/extensions/send-mail';
 
 const app = express();
 
@@ -14,29 +16,12 @@ app.set('view engine','ejs');
 app.use('/public',express.static(path.join(__dirname, 'static')));
 
 //Custom middlewares
-
 /**
  * Get random quote, appends in app.get('quote')
  */
-app.use(async (req, res, next) => {
-  const quoteApi = 'https://quotes.rest/qod';
-  const quote = app.get('quote');
-
-  if (!quote) {
-    // const quoteRequest = await axios.get(quoteApi);
-    // const quoteResponse = {
-    //   quote: quoteRequest.data.contents.quotes[0].quote,
-    //   author: quoteRequest.data.contents.quotes[0].author
-    // }
-
-    const quoteResponse = {
-      quote: 'I think it is often easier to make progress on mega-ambitious dreams. Since no one else is crazy enough to do it, you have little competition. In fact, there are so few people this crazy that I feel like I know them all by first name.',
-      author: 'Larry Page'
-    }
-    app.set('quote', quoteResponse);
-  }
-  next();
-});
+app.use(generateQuote);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Available Routes
 app.get('/', async (req, res) => res.render('index', {
@@ -52,10 +37,13 @@ app.get('/about', (req, res) => res.render('about', {
 }));
 
 app.get('/contact', (req, res) => res.render('contact', {
-  navLinks,
-  quoteResponse: app.get('quote'),
-  title: 'Contact Me'
-}));
+    param: req.query.message,
+    navLinks,
+    quoteResponse: app.get('quote'),
+    title: 'Contact Me'
+  }));
+
+app.post('/send-mail', sendEmail);
 
 app.get('*', (req, res) => res.redirect('/'));
 
